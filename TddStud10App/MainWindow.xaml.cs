e0fa37a7f3ed2f0ebdc713dc1f7cd34d7652101b
Partho P. Das <parthopdas@live.com>
2016-12-15 05:23:27 +0000

    Feature/60 (#65) - Consolidating components into their top level folders
    * Consolidating the VS component
    
    * Consolidating the Test component
    
    * Fixing build break
    
    * Fixing build break
    
    * Consolidating the Engine component

---
+++ TddStud10App/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace R4nd0mApps.TddStud10.Hosts.Console.TddStud10App
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
